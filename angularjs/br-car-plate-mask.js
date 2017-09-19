b39d8c2ed6e8180d19a0655a98e169178b9fc675
Vitor Lopes <vitor@mixd.com.br>
2017-09-19 15:22:19 -0300

    Improving mobile keyboard user experience in brazilian car plate input mask

--- angularjs/br-car-plate-mask.js
+++ angularjs/br-car-plate-mask.js
@@ -2,7 +2,7 @@ angular.module('directives')
 
     .directive('brCarPlateMask', plateMask);
 
-function plateMask() {
+function plateMask($timeout) {
 
     var ddo = {};
 
@@ -12,52 +12,59 @@ function plateMask() {
 
     ddo.link = function (scope, element, attrs, ctrl) {
 
-        var input = angular.element(element[0]);
-        var currentInputValue = '';
+        var input = angular.element(element[0]),
+            typedValue = '',
+            cleanValue = '',
+            plateLimitLength = 8;
 
         input.on('input', onInput);
 
+        input.on('focus', changeKeyboard);
+
+        element.bind('keydown keypress', onKeyPress);
+
         function onInput() {
 
-            var inputRawValue = input[0].value || '';
+            typedValue = input[0].value || '';
 
-            var inputValue = input[0].value.replace(/\W/g, '') || '';
+            cleanValue = input[0].value.replace(/\W/g, '') || '';
 
-            var plateLimitLength = 8;
+            // Check the plate pattern and clear the last character if it doesn't match
+            if (!isPlatePattern(cleanValue)) {
 
-            if (!isPlatePattern(inputValue)) {
-
-                setUpperCasedInputValue(inputRawValue.slice(0, inputRawValue.length - 1));
+                setUpperCasedInputValue(typedValue.slice(0, typedValue.length - 1));
                 return;
             }
 
+            // Just set uppercase
             setUpperCasedInputValue(input[0].value);
 
-            if (inputRawValue.length > plateLimitLength) {
+            // If try to set more characters than the limit, remove the last character
+            if (typedValue.length > plateLimitLength) {
 
-                setUpperCasedInputValue(inputRawValue.slice(0, plateLimitLength));
+                setUpperCasedInputValue(typedValue.slice(0, plateLimitLength));
                 return;
             }
 
-            if (inputValue.length > 3) {
+            if (cleanValue.length === 3 && input[0].type === 'text') {
 
-                var stringPrefix = inputValue.slice(0, 3);
+                setUpperCasedInputValue(cleanValue + '-');
 
-                var stringSuffix = inputValue.slice(stringPrefix.length);
+                $timeout(moveCursorToEnd, 1);
+            }
+
+            if (cleanValue.length === 4 && input[0].type === 'tel') {
+
+                var stringPrefix = cleanValue.slice(0, 3);
+
+                var stringSuffix = cleanValue.slice(stringPrefix.length);
 
                 setUpperCasedInputValue(stringPrefix + '-' + stringSuffix);
+
+                $timeout(moveCursorToEnd, 1);
             }
 
-            if (inputRawValue < currentInputValue) {
-
-                var suffix = inputRawValue.slice(3);
-
-                if (suffix === '-') {
-
-                    setUpperCasedInputValue(inputRawValue.slice(0, 3));
-                }
-            }
-
+            // Set input validation
             ctrl.$validators.pattern = function isPlate(modelValue) {
 
                 if (ctrl.$isEmpty(modelValue)) {
@@ -69,46 +76,83 @@ function plateMask() {
             };
         }
 
+        function onKeyPress(pressEvent) {
+
+            console.log('keypress', cleanValue.length);
+
+            if (pressEvent.keyCode !== 8) { // backspace
+
+                return;
+            }
+
+            if (cleanValue.length <= 3) {
+
+                input[0].blur();
+                input[0].focus();
+            }
+        }
+
         function setUpperCasedInputValue(value) {
 
             input[0].value = value.toUpperCase();
-
-            currentInputValue = input[0].value;
         }
 
-        function isPlatePattern(inputValue) {
+        function moveCursorToEnd() {
 
-            if (inputValue.length === 1 && /\D/.test(inputValue)) {
+            input[0].setSelectionRange(8, 8);
+            input[0].blur();
+            input[0].focus();
+        }
+
+        function changeKeyboard() {
+
+            console.log('focus', typedValue.length);
+
+            if (input[0].value.length <= 3) {
+
+                input[0].type = 'text';
+                return;
+            }
+
+            if (input[0].value.length > 3) {
+
+                input[0].type = 'tel';
+            }
+        }
+
+        function isPlatePattern(cleanValue) {
+
+            if (cleanValue.length === 1 && /\D/.test(cleanValue)) {
 
                 return true;
             }
 
-            if (inputValue.length === 2 && /\D{2}/.test(inputValue)) {
+            if (cleanValue.length === 2 && /\D{2}/.test(cleanValue)) {
 
                 return true;
             }
 
-            if (inputValue.length === 3 && /\D{3}/.test(inputValue)) {
+            if (cleanValue.length === 3 && /\D{3}/.test(cleanValue)) {
 
                 return true;
             }
 
-            if (inputValue.length === 4 && /\D{3}\d/.test(inputValue)) {
+            if (cleanValue.length === 4 && /\D{3}\d/.test(cleanValue)) {
 
                 return true;
             }
 
-            if (inputValue.length === 5 && /\D{3}\d{2}/.test(inputValue)) {
+            if (cleanValue.length === 5 && /\D{3}\d{2}/.test(cleanValue)) {
 
                 return true;
             }
 
-            if (inputValue.length === 6 && /\D{3}\d{3}/.test(inputValue)) {
+            if (cleanValue.length === 6 && /\D{3}\d{3}/.test(cleanValue)) {
 
                 return true;
             }
 
-            if (inputValue.length === 7 && /\D{3}\d{4}/.test(inputValue)) {
+            if (cleanValue.length === 7 && /\D{3}\d{4}/.test(cleanValue)) {
 
                 return true;
             }
